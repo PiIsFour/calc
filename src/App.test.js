a4b2b98eb75a0deaa95835967ba95bb025ac6da9
PiIsFour <PiIsFour@users.noreply.github.com>
2018-03-21 12:01:53 +0100

    add enzyme for testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { shallow } from 'enzyme'
+import enzymeToJson from 'enzyme-to-json'
 
-it('renders without crashing', () => {
-	const div = document.createElement('div')
-	ReactDOM.render(<App />, div)
-	ReactDOM.unmountComponentAtNode(div)
+describe('app', () => {
+	it('renders without crashing', () => {
+		const div = document.createElement('div')
+		ReactDOM.render(<App />, div)
+		ReactDOM.unmountComponentAtNode(div)
+	})
+
+	it('snapshot matches', () => {
+		const wrapper = shallow(<App />)
+		expect(enzymeToJson(wrapper)).toMatchSnapshot()
+	})
 })
